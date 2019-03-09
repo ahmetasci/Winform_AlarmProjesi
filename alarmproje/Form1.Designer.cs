d1b56c3ea4661902bb4284044cd777cec660ff4b
Ahmet AŞÇI <ahmetasci94@gmail.com>
2019-03-09 03:47:38 +0300

    firstclass

---
+++ alarmproje/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace alarmproje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btn_Kapat = new System.Windows.Forms.Button();
+            this.btn_Acma = new System.Windows.Forms.Button();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tarih";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Alarm Saati";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Alar Açıklama";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(191, 58);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(191, 124);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 4;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(191, 181);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(164, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "depositphotos_114820288-stock-illustration-alarm-icon-security-and-insurance.jpg");
+            this.ımageList1.Images.SetKeyName(1, "Aha-Soft-Large-Time-Alarm-clock.ico");
+            this.ımageList1.Images.SetKeyName(2, "Aha-Soft-Large-Time-Alarm-clock-disabled.ico");
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(582, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(147, 133);
+            this.panel1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::alarmproje.Properties.Resources.Alarm_resim;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 86);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Alarm";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // btn_Kapat
+            // 
+            this.btn_Kapat.ImageKey = "Aha-Soft-Large-Time-Alarm-clock-disabled.ico";
+            this.btn_Kapat.ImageList = this.ımageList1;
+            this.btn_Kapat.Location = new System.Drawing.Point(669, 233);
+            this.btn_Kapat.Name = "btn_Kapat";
+            this.btn_Kapat.Size = new System.Drawing.Size(75, 57);
+            this.btn_Kapat.TabIndex = 8;
+            this.btn_Kapat.UseVisualStyleBackColor = true;
+            this.btn_Kapat.Click += new System.EventHandler(this.btn_Kapat_Click);
+            // 
+            // btn_Acma
+            // 
+            this.btn_Acma.ImageKey = "Aha-Soft-Large-Time-Alarm-clock.ico";
+            this.btn_Acma.ImageList = this.ımageList1;
+            this.btn_Acma.Location = new System.Drawing.Point(559, 233);
+            this.btn_Acma.Name = "btn_Acma";
+            this.btn_Acma.Size = new System.Drawing.Size(75, 57);
+            this.btn_Acma.TabIndex = 7;
+            this.btn_Acma.UseVisualStyleBackColor = true;
+            this.btn_Acma.Click += new System.EventHandler(this.btn_Acma_Click);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_Kapat);
+            this.Controls.Add(this.btn_Acma);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Acma;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Button btn_Kapat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ alarmproje/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace alarmproje
+{
+    /*
+    Bir form üzerinde girilen tarih ve saat (saat ve dakika) geldiğinde hatırlatacak bir alarm uygulaması hazırlayınız.
+Form üzerinde tarih seçimi için DateTimePicker, saat ve dakika girişi için MaskedTextBox kullanınız.
+Ayrıca çok satırlı bir hatırlatma notu girilecek metin alanı oluşturunuz.
+Form üzerinde Alarm Kur butonuna tıklandığında form gizlenip, ikon halinde beklemeli, alarm zamanı geldiğinde ekran ön plana gelmeli. O anda ekranda başka açık programlar olabileceği için form her şeyin önüne gelmeli.
+Alarm kurulmadan önce ve sonra form açıkken şimdiki zaman saat, dakika ve saniye bilgisi form başlığında gösterilmeli.
+Alarm başladığında tekrar tekrar bir ses çalınmalı, ekranda hatırlatma notu alanının arka planı kırmızı ve beyaz renkler arasında değişmeli ve form üzerindeki bir zil resmi kendi etrafında küçük hareketler yaparak sarsılma etkisi oluşturulmalı. Bu hareketler için Timer nesnesi kullanınız.
+Form üzerinde Alarmı durdur butonuna tıklanınca Alarm hareketleri ve ses durdurulmalı.
+Ses çalmak için SoundPlayer sınıfını ve C:\Windows klasöründeki WAV dosyalarından birisini kullanabilirsiniz.
+
+    */
+    public partial class Form1 : Form
+    {
+        SoundPlayer ses = new SoundPlayer();
+        bool renkChanged = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           
+            if (maskedTextBox1.Text == DateTime.Now.ToString("HH:mm"))
+            {
+                
+                string alarmmuzigi = Application.StartupPath + "\\alarm.wav"; //alarm sesinin yolunu verdik
+                ses.SoundLocation = alarmmuzigi; 
+                ses.Play();
+                timer3.Start();
+                Random rnd = new Random(); //random sınıfından instance aldık
+                pictureBox1.Top = rnd.Next(panel1.ClientSize.Height/3); //Panel içerisinde alarm resmi rastgele gidip gelecek
+                pictureBox1.Left = rnd.Next(panel1.ClientSize.Width/3);
+                this.Visible = true; // alarm çalmaya başladığında arkaplandaki ikon ekrana sayfa şeklinde gelecek. 
+                
+            }
+        }
+        private void btn_Acma_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            //Ekranın görünürlüğünü kapattık.İkonu arka plana attık
+            timer2.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            this.Text = DateTime.Now.ToString();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {//notifyIcon toolboxtan ekledik. İkon halinde göstermeye yarar.
+            this.Visible = true;
+            //ikona çift tıklarsak ekranın görünürlüğünü açar.
+        }
+
+        private void btn_Kapat_Click(object sender, EventArgs e)
+        {//btn_Kapat klik eventinde 
+            timer2.Stop();
+            ses.Stop(); // alarm sesini kapattık
+            timer3.Stop();
+            
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (renkChanged==false)
+            {
+                richTextBox1.BackColor = Color.White; //richtextboxtaki yazı ve arkaplan rengi kırmızı beyaz arasında alarm çaldığı sürece değişecek
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.BackColor = Color.Red;
+                richTextBox1.ForeColor = Color.White;
+                renkChanged = true;
+            }
+            else
+            {
+                richTextBox1.BackColor = Color.Red;
+                richTextBox1.ForeColor = Color.White;
+                richTextBox1.BackColor = Color.White;
+                richTextBox1.ForeColor = Color.Red;
+                renkChanged = false;
+            }
+        }
+    }
+}
